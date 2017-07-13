e97a34d8850df0a5ac0858807d391a14af57c6de
Gene Harvey <gharveymn@gmail.com>
2017-07-13 18:32:18 -0500

    transposed some stuff, didnt work, reverting

--- utils/MakeStaggeredGridsBox.m
+++ utils/MakeStaggeredGridsBox.m
@@ -120,89 +120,89 @@ function [grids,filtering,par] = MakeStaggeredGridsBox(par)
 
 	%make u velocity grid offset in the y direction
 	%inner
-	uxinit = (limits(1)+h:par.h:limits(2)-h)';
-	uyinit = (limits(3)+h/2:par.h:limits(4)-h/2)';
-	nxm1 = numel(uxinit);
-	ny = numel(uyinit);
+	uxinit = (limits(1)+h/2:par.h:limits(2)-h/2)';
+	uyinit = (limits(3)+h:par.h:limits(4)-h)';
+	nx = numel(uxinit);
+	nym1 = numel(uyinit);
 	uxlimcoords = xlimcoords;
 	uylimcoords = ylimcoords;
 	%hardcoded for box
 	for i=1:5	
 		if((i==2 || i==3))
-			uxlimcoords(i) = uxlimcoords(i)-h;
+			uxlimcoords(i) = uxlimcoords(i)-h/2;
 		else
-			uxlimcoords(i) = uxlimcoords(i)+h;
+			uxlimcoords(i) = uxlimcoords(i)+h/2;
 		end
 		
 		if(i>1 && i<4)
-			uylimcoords(i) = uylimcoords(i)-h/2;
+			uylimcoords(i) = uylimcoords(i)-h;
 		else
-			uylimcoords(i) = uylimcoords(i)+h/2;
+			uylimcoords(i) = uylimcoords(i)+h;
 		end
 	end
-	[ugrids,ufiltering] = createGridsInner(uxinit,uyinit,nxm1,ny,uxlimcoords,uylimcoords,h,par);
+	[ugrids,ufiltering] = createGridsInner(uxinit,uyinit,nx,nym1,uxlimcoords,uylimcoords,h,par);
 
 	%outer
-	uxinit = (limits(1):par.h:limits(2))';
-	uyinit = (limits(3)-h/2:par.h:limits(4)+h/2)';
-	nxp1 = numel(uxinit);
-	nyp2 = numel(uyinit);
+	uxinit = (limits(1)-h/2:par.h:limits(2)+h/2)';
+	uyinit = (limits(3):par.h:limits(4))';
+	nxp2 = numel(uxinit);
+	nyp1 = numel(uyinit);
 	uxlimcoords = xlimcoords;
 	uylimcoords = ylimcoords;
 	%hardcoded for symch
 	for i=1:5
-		if(i>1 && i<4)
-			uylimcoords(i) = uylimcoords(i)+h/2;
+		if(i==2 || i==3)
+			uxlimcoords(i) = uxlimcoords(i)+h/2;
 		else
-			uylimcoords(i) = uylimcoords(i)-h/2;
+			uxlimcoords(i) = uxlimcoords(i)-h/2;
 		end
 	end
 	
-	[ugrids,ufiltering] = createGridsOuter(uxinit,uyinit,nxp1,nyp2,uxlimcoords,uylimcoords,h,ugrids,ufiltering,par);
+	[ugrids,ufiltering] = createGridsOuter(uxinit,uyinit,nxp2,nyp1,uxlimcoords,uylimcoords,h,ugrids,ufiltering,par);
 	
 	%V
 	%------------------------------------------------
 
 	%make v velocity grid offset in the x direction
 	%inner
-	vxinit = (limits(1)+h/2:par.h:limits(2)-h/2)';
-	vyinit = (limits(3)+h:par.h:limits(4)-h)';
-	nx = numel(vxinit);
-	nym1 = numel(vyinit);
+	vxinit = (limits(1)+h:par.h:limits(2)-h)';
+	vyinit = (limits(3)+h/2:par.h:limits(4)-h/2)';
+	nxm1 = numel(vxinit);
+	ny = numel(vyinit);
 	vxlimcoords = xlimcoords;
 	vylimcoords = ylimcoords;
 	%hardcoded for box
 	for i=1:5
 		if(i==2 || i==3)
-			vxlimcoords(i) = vxlimcoords(i)-h/2;
+			vxlimcoords(i) = vxlimcoords(i)-h;
 		else
-			vxlimcoords(i) = vxlimcoords(i)+h/2;
+			vxlimcoords(i) = vxlimcoords(i)+h;
 		end
 		
 		if(i>1 && i<4)
-			vylimcoords(i) = vylimcoords(i)-h;
+			vylimcoords(i) = vylimcoords(i)-h/2;
 		else
-			vylimcoords(i) = vylimcoords(i)+h;
+			vylimcoords(i) = vylimcoords(i)+h/2;
 		end
 	end
-	[vgrids,vfiltering] = createGridsInner(vxinit,vyinit,nx,nym1,vxlimcoords,vylimcoords,h,par);	
+	[vgrids,vfiltering] = createGridsInner(vxinit,vyinit,nxm1,ny,vxlimcoords,vylimcoords,h,par);	
 
 	%outer
-	vxinit = (limits(1)-h/2:par.h:limits(2)+h/2)';
-	vyinit = (limits(3):par.h:limits(4))';
-	nxp2 = numel(vxinit);
-	nyp1 = numel(vyinit);
+	vxinit = (limits(1):par.h:limits(2))';
+	vyinit = (limits(3)-h/2:par.h:limits(4)+h/2)';
+	nxp1 = numel(vxinit);
+	nyp2 = numel(vyinit);
 	vxlimcoords = xlimcoords;
 	vylimcoords = ylimcoords;
 	%hardcoded for box
 	for i=1:5
-		if(i==2 || i==3)
-			vxlimcoords(i) = vxlimcoords(i)+h/2;
+		if(i>1 && i<4)
+			vylimcoords(i) = vylimcoords(i)+h/2;
 		else
-			vxlimcoords(i) = vxlimcoords(i)-h/2;
+			vylimcoords(i) = vylimcoords(i)-h/2;
 		end
 	end
-	[vgrids,vfiltering] = createGridsOuter(vxinit,vyinit,nxp2,nyp1,vxlimcoords,vylimcoords,h,vgrids,vfiltering,par);	
+	[vgrids,vfiltering] = createGridsOuter(vxinit,vyinit,nxp1,nyp2,vxlimcoords,vylimcoords,h,vgrids,vfiltering,par);	
 
 	%------------------------------------------------
 	
