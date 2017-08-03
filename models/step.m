d8c92d40b9ef9c92a9d7e68a13c7d542369a2a05
Gene Harvey <gharveymn@gmail.com>
2017-08-03 06:19:19 -0500

    sync

--- models/step.m
+++ models/step.m
@@ -4,10 +4,10 @@ function par = step(par)
 	par.bcfunc = @BCConIO;
 	par.hx = 0.25;
 	par.hy = 0.05;
-	par.Re = 1000;
-	par.plotoniter = 10;
+	par.Re = 100;
+	par.plotoniter = 1000;
 	par.quivVectSca = .1*(sqrt(par.hx^2 + par.hy^2)/0.05);
-	par.dt = 0.1;
+	par.dt = 0.01;
 	par.tf = 1000;
 	
 end
